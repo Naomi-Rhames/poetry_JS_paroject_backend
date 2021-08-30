daddb48dad3c8926c5b3bfc68ba30dba89e36d9d
Naomi-Rhames <naomirhames2020@gmail.com>
2021-08-30 14:43:18 -0500

    poem model

---
+++ db/migrate/20210830182126_create_poems.rb
@@ -0,0 +1,14 @@
+class CreatePoems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :poems do |t|
+      t.string :title
+      t.string :author
+      t.string :stanza
+      t.string :genre
+      t.string :image_url
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
