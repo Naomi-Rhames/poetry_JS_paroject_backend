63d2adb61c40d06658ff160621dedfe46543a196
Naomi-Rhames <naomirhames2020@gmail.com>
2021-08-30 20:14:41 -0500

    poem controller index,create

--- app/controllers/api/v1/poems_controller.rb
+++ app/controllers/api/v1/poems_controller.rb
@@ -1,4 +1,27 @@
 class Api::V1::PoemsController < ApplicationController
 
     
+    def create
+        poem = Poem.new(poem_params)
+        if poem.save
+            render json: poem, status: :accepted
+        else
+            render json: {errors: poem.error.full_messages}, status: :unprocessible_entity #error 422
+        end
+    end
+
+
+    def index
+        poems = Poem.all
+        render json: poems #making sure that rails can read the data from JSON
+    end
+
+
+    private
+    def poem_params
+         #.require is requiring the top level hash
+         #.permit is perrmitting the attributes
+        params.require(:poem).permit(:title, :name, :stanza, :image_url, :category_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
 
     namespace :v1 do
-      resources :poems, only: [:index]
+      resources :poems, only: [:index, :create]
  
     end
   end
