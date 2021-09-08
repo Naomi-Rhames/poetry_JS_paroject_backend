eb7be8436d0b5ee682d14da84c2dc6ce770b738f
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-08 10:01:52 -0500

    add update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
 
     namespace :v1 do
-      resources :poems, only: [:index, :create]
+      resources :poems, only: [:index, :create, :update]
       resources :categories, only: [:index]
     end
   end
