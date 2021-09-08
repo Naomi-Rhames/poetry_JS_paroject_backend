564b56b2e689801a418b2547a7c7190f684b79ed
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-08 10:02:12 -0500

    validation

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,3 +1,9 @@
 class Poem < ApplicationRecord
     belongs_to :category
+
+    validates  :title, presence: true
+    validates  :author, presence: true
+    validates  :image_url, presence: true
+    validates  :stanza, presence: true
+
 end
