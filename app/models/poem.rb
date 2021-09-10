9b1f0903a9ace93195150f435a03bac5976c37b3
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-10 00:56:39 -0500

    Fixed PATCH, CSS

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -2,6 +2,7 @@ class Poem < ApplicationRecord
     belongs_to :category
 
     validates  :title, presence: true
+    validates :genre, presence: true
     validates  :author, presence: true
     validates  :image_url, presence: true
     validates  :stanza, presence: true
