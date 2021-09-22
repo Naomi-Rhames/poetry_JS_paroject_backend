b39e35cccce1e5324a41a5fc8a768bd7f34b72eb
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-22 13:04:35 -0500

    fixed relationships

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,7 +1,6 @@
 class Poem < ApplicationRecord
     belongs_to :category
-    belongs_to :user
-    
+
     validates  :title, presence: true
     validates :genre, presence: true
     validates  :author, presence: true
